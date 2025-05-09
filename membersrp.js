8c2fa1061bab0af0ed803e2c6ed92ca5830602ea
rrive0 <113113005+rrive0@users.noreply.github.com>
2025-05-09 13:26:08 +0700

    Update membersrp.js

--- membersrp.js
+++ membersrp.js
@@ -4,7 +4,11 @@ const members = [
   { name: "Percy Gxw", fbId: "perq.diamondonsnow#" },
   { name: "Wayla Gxw", fbId: "wayla.godmyhxnd#" },
   { name: "Kinz Gxw", fbId: "100080485166340#" },
-  { name: "Kxii Vincere", fbId: "100085538075706#" }
+  { name: "Kxii Vincere", fbId: "100085538075706#" },
+  { name: "Mochiix Gxw", fbId: "hdNrvXugurtA6dOs#" },
+  { name: "Jing Gxw", fbId: "jing.gxw#" },
+  { name: "Josefine Gxw", fbId: "100077494847829#" },
+  { name: "Mindjia Lamlukka", fbId: "61550306857972#" }
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
