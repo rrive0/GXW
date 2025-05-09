93e69ce69dc41955bbe2bc3cd6b111d862a1d331
rrive0 <113113005+rrive0@users.noreply.github.com>
2025-05-09 13:30:58 +0700

    Update membersrp.js

--- membersrp.js
+++ membersrp.js
@@ -5,7 +5,7 @@ const members = [
   { name: "Wayla Gxw", fbId: "wayla.godmyhxnd#" },
   { name: "Kinz Gxw", fbId: "100080485166340#" },
   { name: "Kxii Vincere", fbId: "100085538075706#" },
-  { name: "Mochiix Gxw", fbId: "hdNrvXugurtA6dOs#" },
+  { name: "Mochiix Gxw", fbId: "Mochiixz#" },
   { name: "Jing Gxw", fbId: "jing.gxw#" },
   { name: "Josefine Gxw", fbId: "100077494847829#" },
   { name: "Mindjia Lamlukka", fbId: "61550306857972#" }
